cfb7da23c16b15c6b5114539876f63956e0698be
Michael Emmi <michael.emmi@gmail.com>
2013-11-08 14:36:24 +0100

    Printing echoed expressions in trace parser.

--- bin/boogie-trace-parser.rb
+++ bin/boogie-trace-parser.rb
@@ -65,13 +65,18 @@ module BoogieTraceParser
     
     following(/Boogie program verifier version/)
     see(/This assertion might not hold./)
-    see(/Execution trace:/)    
-    procedure
+    trace
+    see(/Boogie program verifier finished/)
 
     "digraph G { \
       \n  node [shape = record];
-      \n  #{tree * "\n  "} \
+      \n  #{@tree * "\n  "} \
     \n}"
+  end
+  
+  def trace
+    see(/Execution trace:/)    
+    _ = procedure
   end
   
   def procedure(name = nil)
@@ -87,7 +92,7 @@ module BoogieTraceParser
       next if line.match /Inlined call to procedure (.*) begins/ do |m|
         stmts << "call #{m[1]}"
         child = procedure m[1]
-        tree << "#{me} -> #{child};"
+        @tree << "#{me} -> #{child};"
       end
       
       next if line.match /value = T@\$mop!val!(\d+)/ do |m|
@@ -97,40 +102,62 @@ module BoogieTraceParser
         val  = see(/value = (.*)/){|m| m[1].gsub(/[() ]/,"").to_i}
 
         if kind == :read
-          stmts << "read M[#{addr}] =&gt; #{val}"
+          stmts << "read M[#{addr}] = #{val}"
         else
-          stmts << "M[#{addr}] := #{val}"
+          stmts << "write M[#{addr}] := #{val}"
         end
       end
       
       next if line.match /value = (.*)/ do |m|
-        val = m[1].gsub(/[()]/,"")
+        # val = m[1].gsub(/[() ]/,"")
+        rec = @exprs.shift
         if round_known then
-          stmts << "val: #{val}"
+          # stmts << "val: #{val}"
+          stmts << "echo #{rec[:expr]}:#{rec[:type]} = #{rec[:val]}"
         else
-          if stmts.last then stmts.last << " ROUND #{val} "
-          else stmts << " ROUND #{val} "
+          if stmts.last then stmts.last << " ROUND #{rec[:val]} "
+          else stmts << " ROUND #{rec[:val]} "
           end
           round_known = true
         end
       end
       
-      next if line.match /.*\.bpl\(\d+,\d+\): ~yield/ do
+      next if line.match /(\S+\.bpl)\((\d)+,(\d)+\): ~yield/ do |m|
+        @block = {file: m[1], line: m[2].to_i, col: m[3].to_i}
+        @exprs = recorded_exprs
         round_known = false
         stmts.last << "|"
       end
       
-      next if line.match /.*\.bpl\(\d+,\d+\): .*/ do
+      next if line.match /(\S+\.bpl)\((\d+),(\d+)\): .*/ do |m|
+        @block = {file: m[1], line: m[2].to_i, col: m[3].to_i}
+        @exprs = recorded_exprs
         true
       end
       
       err "unexpected line: #{line}"
     end
     
-    tree << "#{me} [label=\"{#{name || "top"} | #{stmts * '\l'} }\"];"
+    @tree << "#{me} [label=\"{#{name || "top"} | #{stmts * '\l'} }\"];"
     return me
   end
   
+  def recorded_exprs
+    num_vals = @lines.index{|l| l !~ /value = .*/}
+    
+    line_no = 0
+    exprs = []
+    File.new(@block[:file],'r').each_line do |line|
+      break unless exprs.size < num_vals
+      next if (line_no += 1) < @block[:line]
+      line.match /call\s+boogie_si_record_(\S+)\s*\((.*)\)\s*;/ do |m|
+        val = @lines[exprs.size].match(/value = (.*)/){|m| m[1].gsub(/[() ]/,"")}
+        exprs << {expr: m[2], type: m[1], val: val}
+      end
+    end
+    return exprs
+  end
+
 end
 
 if __FILE__ == $0 then
